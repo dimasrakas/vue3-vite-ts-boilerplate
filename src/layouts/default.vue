274614d0b1e045017c629da145d747d860577e12
dimasrakas <dimasrakas@gmail.com>
2022-02-14 17:21:00 +0700

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="max-w-7xl mx-auto p-6">
+    <router-view />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-8xl">Dimas Raka Septiawan</h1>
+  </div>
+</template>
